760868f8d3b60145d72c0daa92eef9228c5d0db4
UserPL135Pc02 <UserPL135Pc02@SLFRPL135PC02.ensino.atec.local>
2016-10-26 12:05:56 +0100

    Last

--- src/exercicioBalanco.java
+++ src/exercicioBalanco.java
@@ -1,21 +1,30 @@
 
 public class exercicioBalanco {
-
+	int totalReceitas;
+	int totalDespesas;
+	
 	public exercicioBalanco() {
 		
-		int quotas = 300, 
-			recBaile = 200, 
-			recBar = 250;
-		int totalReceitas = quotas + recBaile + recBar;
-		
-		int agua = 100,
-			luz = 60,
-			telefone = 70;
-		int totalDespesas = agua + luz + telefone;
+		calcularReceitas();
+		calcularDespesas();
 		
 		System.out.println("O total de receitas é: €" + totalReceitas);
 		System.out.println("O total de despesas é: €" + totalDespesas);
 		System.out.println("O balanço final é: €" + (totalReceitas - totalDespesas));
+	}
+	
+	public void calcularReceitas() {
+		int quotas = 300, 
+			recBaile = 200, 
+			recBar = 250;
+		totalReceitas = quotas + recBaile + recBar;
+	}
+	
+	public void calcularDespesas() {
+		int agua = 100,
+				luz = 60,
+				telefone = 70;
+		totalDespesas = agua + luz + telefone;
 	}
 
 }
